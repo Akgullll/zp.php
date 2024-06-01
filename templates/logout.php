19dd66c1cb4ec405de6d74c1e7455868dab80242
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-01 06:26:47 +0200

    logout.php
    logout.php

--- templates/logout.php
+++ templates/logout.php
@@ -7,6 +7,7 @@ include('partials/header.php');
         <div class="row">
             <div class="col-100 text-left">
                 <?php
+                // Odhlásenie používateľa z aktuálnej relácie
                 unset($_SESSION['logged_in']);
                 unset($_SESSION['is_admin']);
                 ?>
@@ -20,4 +21,5 @@ include('partials/header.php');
 <?php
 include('partials/footer.php');
 ?>
+
 
