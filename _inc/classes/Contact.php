646e9e882795afa21361f7990745cee689216474
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-01 06:57:32 +0200

    Contact.php
    Contact.php

--- _inc/classes/Contact.php
+++ _inc/classes/Contact.php
@@ -1,4 +1,13 @@
 <?php
+/* Môžeme vytvoriť tabuľku pomocou vzorca
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `message` text NOT NULL,
+  `acceptance` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovak_ci;*/
+
 
 class Contact extends Database {
 
@@ -9,7 +18,9 @@ class Contact extends Database {
     }
 
     public function insert() {
+        // Overenie, či existuje pripojenie k databáze
         if ($this->db) {
+            // Spracovanie dát z formulára
             if (isset($_POST['contact_submitted'])) {
                 $data = array(
                     'contact_name' => $_POST['name'],
@@ -19,32 +30,35 @@ class Contact extends Database {
                 );
 
                 try {
+                    // Pripravenie a vykonanie SQL dotazu pre vloženie kontaktu do databázy
                     $query = "INSERT INTO contact (name, email, message, acceptance) 
                               VALUES (:contact_name, :contact_email, :contact_message, :contact_acceptance)";
                     $query_run = $this->db->prepare($query);
                     $query_run->execute($data);
                 } catch (PDOException $e) {
-                    echo $e->getMessage();
+                    echo $e->getMessage(); // Výpis chybovej správy v prípade chyby
                 }
             }
         } else {
-            echo 'Nemáme spojenie';
+            echo 'No connection'; // Výpis správy v prípade chyby pripojenia k databáze
         }
     }
 
     public function select() {
         try {
+            // Vykonanie SQL dotazu na výber všetkých kontaktov z tabuľky contact
             $sql = "SELECT * FROM contact";
             $query = $this->db->query($sql);
-            $contacts = $query->fetchAll();
-            return $contacts;
+            $contacts = $query->fetchAll(); // Získanie výsledkov dotazu
+            return $contacts; // Vrátenie všetkých kontaktov
         } catch (PDOException $e) {
-            echo $e->getMessage();
+            echo $e->getMessage(); // Výpis chybovej správy v prípade chyby
         }
     }
 
     public function delete($contact_id) {
         try {
+            // Pripravenie a vykonanie SQL dotazu na vymazanie kontaktu z databázy podľa zadaného ID
             $data = array(
                 'contact_id' => $contact_id
             );
@@ -52,10 +66,11 @@ class Contact extends Database {
             $query_run = $this->db->prepare($query);
             $query_run->execute($data);
         } catch (PDOException $e) {
-            echo $e->getMessage();
+            echo $e->getMessage(); // Výpis chybovej správy v prípade chyby
         }
     }
     
 
 }
 ?>
+
