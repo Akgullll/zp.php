28384766b3e0f729975a43f9e08de655149e4277
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-01 06:52:30 +0200

    Menu.php
    Menu.php

--- _inc/classes/Menu.php
+++ _inc/classes/Menu.php
@@ -1,25 +1,25 @@
 <?php
 
-    class Menu{
+class Menu{
 
-        private $pages;
+    private $pages; // Premenná pre uchovávanie zoznamu stránok a ich URL adries
 
-        public function __construct($pages){
-            $this->pages = $pages;
-        }
-
-        function generate_menu(): string{
-            $menu_items = ''; 
-            
-          
-            foreach($this->pages as $page_name => $page_url){
-                
-                $menu_items .= '<li><a href="' . $page_url . '">' . $page_name . '</a></li>';
-            }
-        
-           
-            return $menu_items;
-        }
+    public function __construct($pages){
+        $this->pages = $pages; // Inicializácia premenné $pages v konštruktore
     }
+
+    // Metóda na generovanie navigačného menu
+    function generate_menu(): string{
+        $menu_items = ''; // Inicializácia premennej pre uloženie jednotlivých položiek menu
+        
+        // Prechádzanie cez zoznam stránok a ich URL adries
+        foreach($this->pages as $page_name => $page_url){
+            // Vytvorenie HTML kódu pre každú stránku ako odkaz s názvom a URL adresou
+            $menu_items .= '<li><a href="' . $page_url . '">' . $page_name . '</a></li>';
+        }
+    
+        return $menu_items; // Vrátenie reťazca obsahujúceho kompletné navigačné menu
+    }
+}
 
 ?>
