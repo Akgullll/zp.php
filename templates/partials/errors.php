4f42ba52ea517b76411749dca58a1e95aa2516d6
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-01 06:31:29 +0200

    errors.php

--- templates/partials/errors.php
+++ templates/partials/errors.php
@@ -1,13 +1,16 @@
 <?php
-
+// Definícia funkcie na zobrazenie chybových správ
 function display_errors($errors) {
+    // Kontrola, či pole chýb nie je prázdne
     if (!empty($errors)) {
+        // Začiatok divu pre chybové správy
         echo '<div class="errors">';
+        // Prechádzanie cez pole chýb a výpis jednotlivých chýb ako odstavcov
         foreach ($errors as $error) {
             echo '<p>' . $error . '</p>';
         }
+        // Koniec divu pre chybové správy
         echo '</div>';
     }
 }
-
 ?>
