270066f334c6dc358ebab622f514fe893f5b24f4
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-01 06:28:58 +0200

    home.php
    home.php

--- templates/home.php
+++ templates/home.php
@@ -1,17 +1,16 @@
 <?php
-include_once('partials/header.php');
+include_once('partials/header.php');// Vloženie hlavičky
 ?>    
 <main>
     <?php
-    $headings = array('APPLE', 'APPLE','APPLE');
-    $img_folder = '../assets/img/carus/';
-    $slider = new Slider();
-    $slider->set_headings($headings);
-    $slider->set_img_folder($img_folder);
-    echo $slider->generate_slides();
+    $headings = array('APPLE', 'APPLE','APPLE'); // Nastavenie nadpisov pre prezentáciu
+    $img_folder = '../assets/img/carus/';  // Určenie adresára so zobrazeniami
+    $slider = new Slider(); // Vytvorenie novej inštancie triedy Slider
+    $slider->set_headings($headings);  // Nastavenie nadpisov pre zobrazenia
+    $slider->set_img_folder($img_folder); // Nastavenie adresára so zobrazeniami pre prezentáciu
+    echo $slider->generate_slides();// Generovanie HTML pre prezentáciu a výpis na stránku
     ?>
 </main>
 <?php
-include_once('partials/footer.php');
+include_once('partials/footer.php');//Vloženie päty
 ?> 
-  
