3f38911ca2bb34eb0a071eef4590c14e8f168068
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-01 06:51:59 +0200

    Page.php

--- _inc/classes/Page.php
+++ _inc/classes/Page.php
@@ -1,17 +1,20 @@
 <?php
 
 class Page {
-    private $page_name;
+    private $page_name; // Privátna premenná pre názov stránky
 
+    // Metóda pre nastavenie názvu stránky
     public function set_page_name($page_name) {
         $this->page_name = $page_name;
     }
 
+    // Metóda pre pridanie externých štýlových súborov
     public function add_stylesheet() {
+        // Preddefinované štýly
         $result = '<link rel="stylesheet" href="../assets/css/style.css">
                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
 
-        
+        // Podľa názvu stránky sa pridajú ďalšie štýly
         switch($this->page_name) {
             case 'home':
             case 'kontakt':
@@ -22,23 +25,24 @@ class Page {
             case 'qna':
             case 'thankyou':
             case 'product':
-
                 $result .= '<link rel="stylesheet" href="../assets/css/style2.css">';
                 break;
         }
 
-        return $result;
+        return $result; // Vrátenie reťazca obsahujúceho odkazy na štýlové súbory
     }
 
+    // Metóda pre pridanie skriptov
     public function add_scripts() {
-       
-        return '<script src="../assets/js/app.js"></script>';
+        return '<script src="../assets/js/app.js"></script>'; // Vrátenie reťazca obsahujúceho odkaz na JavaScript súbor
     }
     
+    // Metóda na presmerovanie na domovskú stránku
     public function redirect_homepage() {
-        header("Location: templates/home.php");
-        die("Nepodarilo sa nájsť Domovskú stránku");
+        header("Location: templates/home.php"); // Presmerovanie na domovskú stránku
+        die("Nepodarilo sa nájsť Domovskú stránku"); // Zastavenie ďalšieho vykonávania skriptu a výpis chyby
     }
 }
+
 ?>
 
