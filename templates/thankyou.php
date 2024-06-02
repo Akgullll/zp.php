86a95b1733b070958ab9d57ca6fbd5c75a2d30e2
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-02 03:12:54 +0200

    thankyou.php

--- templates/thankyou.php
+++ templates/thankyou.php
@@ -6,12 +6,6 @@ include('partials/header.php');
         <div class="row">
             <div class="col-100 text-center">
                 <h2>Ďakujeme!(⌒‿⌒)</h2>
-                <?php
-
-                // Vytvorenie inštancie triedy Contact a vloženie údajov
-                $contact_object = new Contact();
-                $contact_object->insert();
-                ?>
             </div>
         </div>
     </section>
