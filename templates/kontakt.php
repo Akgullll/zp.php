54f905bd8b760e55fce3b4cad427cfb166c470a4
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-02 03:01:10 +0200

    Update kontakt.php
    kontakt

--- templates/kontakt.php
+++ templates/kontakt.php
@@ -1,20 +1,47 @@
-<?php include('partials/header.php'); ?>
+<?php
+include('partials/header.php'); 
+
+// Zahrňte potrebné súbory a triedy
+include_once('_inc/classes/Database.php');
+include_once('_inc/classes/Contact.php');
+
+// Vytvorenie objektu triedy Contact
+$contact = new Contact();
+
+// Ak bol formulár odoslaný
+if (isset($_POST['contact_submitted'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $acceptance = isset($_POST['acceptance']) ? 1 : 0;
+
+    // Vytvorenie nového kontaktu
+    if ($contact->create($name, $email, $message, $acceptance)) {
+        header("Location: thankyou.php");
+        exit();
+    } else {
+        echo "Failed to make contact.";
+    }
+}
+?>
+
 <main>
     <section class="container">
         <div class="row">
             <div class="col-100 text-center">
-                <h2>CONTACT US</h2>
-                <form id="contact" action="thankyou.php" method="POST" style="text-align: center;">
-                    <input type="text" placeholder="YOUR NAME" name="name" required><br>
-                    <input type="email" placeholder="YOUR EMAIL" name="email" required><br>
-                    <textarea placeholder="DO YOU HAVE A PROBLEM ABOUT OUR PRODUCTS?" name="message"></textarea><br>
+                <h2>KONTAKTUJTE NÁS</h2>
+                <form id="contact" action="" method="POST" style="text-align: center;">
+                    <input type="text" placeholder="VAŠE MENO" name="name" required><br>
+                    <input type="email" placeholder="VAŠ EMAIL" name="email" required><br>
+                    <textarea placeholder="MÁTE PROBLÉM S NAŠIMI PRODUKTAMI?" name="message"></textarea><br>
                     <input type="checkbox" name="acceptance" value="1" required>
-                    <label> I consent to the processing of my personal data.</label><br>
-                    <input type="submit" style='background-color:  #f9f9f9;'name="contact_submitted" value="🧸">
+                    <label> Súhlasím so spracovaním mojich osobných údajov.</label><br>
+                    <input type="submit" style='background-color: #f9f9f9;' name="contact_submitted" value="🧸">
                 </form>
             </div>
         </div>
     </section>
 </main>
+
 <?php include_once('partials/footer.php'); ?>
 
